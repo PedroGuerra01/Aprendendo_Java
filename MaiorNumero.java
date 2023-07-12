5bab20b3fdd257949dad5fd5383374baa894cc32
Pedro Henrique Guerra <126834928+PedroGuerra01@users.noreply.github.com>
2023-07-12 13:22:06 -0300

    Add files via upload

---
+++ MaiorNumero.java
@@ -0,0 +1,14 @@
+public class MaiorNumero {
+    public static void main(String[] args) {
+        int numero1 = 5;
+        int numero2 = 10;
+
+        if (numero1 > numero2) {
+            System.out.println("O número 1 é maior.");
+        } else if (numero2 > numero1) {
+            System.out.println("O número 2 é maior.");
+        } else {
+            System.out.println("Os números são iguais.");
+        }
+    }
+}
