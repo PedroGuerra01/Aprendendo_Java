c1d35c07600a66c889c5487ffa9df1d2fe7855dd
Pedro Henrique Guerra <126834928+PedroGuerra01@users.noreply.github.com>
2023-07-12 13:32:00 -0300

    Add files via upload

---
+++ NumeroPrimo.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class NumeroPrimo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        boolean ehPrimo = true;
+
+        if (numero <= 1) {
+            ehPrimo = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(numero); i++) {
+                if (numero % i == 0) {
+                    ehPrimo = false;
+                    break;
+                }
+            }
+        }
+
+        if (ehPrimo) {
+            System.out.println(numero + " é um número primo.");
+        } else {
+            System.out.println(numero + " não é um número primo.");
+        }
+
+        scanner.close();
+    }
+}
