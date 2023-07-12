2f05a800ec70c88f6926b5465f055fac5e843ddb
Pedro Henrique Guerra <126834928+PedroGuerra01@users.noreply.github.com>
2023-07-12 13:19:28 -0300

    Add files via upload

---
+++ Soma.java
@@ -0,0 +1,8 @@
+public class Soma {
+    public static void main(String[] args) {
+        int numero1 = 5;
+        int numero2 = 10;
+        int soma = numero1 + numero2;
+        System.out.println("A soma é: " + soma);
+    }
+}
